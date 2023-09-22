ba0ccc34ef15045ea442eda0aea900718f518669
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2023-09-22 17:09:33 +0530

    keys in react act as a unique identifiers

--- App.js
+++ App.js
@@ -4,9 +4,11 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.querySelector('#root')); 
 const head1 = React.createElement('h1', { 
     style: {color:"DarkRed"},
-    id: "heading1"
+    id: "heading",
+    key: "heading1"  // key is used for the React to identify each elements
 }, "Namaste React"); 
-const head2 = React.createElement('h2', {}, "Namaste React");
+const head2 = React.createElement('h2', { key: "heading2"
+}, "Namaste World");
 
 const headingContainer = React.createElement('div', {className: 'headingContainer'}, [head1, head2]);  
 
