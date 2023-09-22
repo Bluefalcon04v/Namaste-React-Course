9a942151e922ae90619f0ecca3b8f252b9472c0c
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2023-09-22 17:45:13 +0530

    functional components and component composition

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// This is a React Element ie. it is just a Object 
 const heads = (
-    <h1 id='heading' key="heading" style={{color:"red",fontFamily:"cursive"}}> Namaste React </h1>
+    <h1 id='heading' key="heading" style={{color:"red",fontFamily:"cursive"}}> Namaste heads </h1>
 );
+
+const Heads2 = () =>(
+    <h2>Namaste heads2</h2>
+)
+
+const HeadingComponent = () =>( // Creating a functional component
+    <div>
+        <h1> Namaste Mom</h1>
+        {heads}        {/* Calling for the "heads" element in the functional components */}
+
+
+        {Heads2()}        {/* Calling for the "heads2" Functional Components in the another functional component also known as component composition*/}
+        {/* OR */}
+        <Heads2/>        {/* Calling for the "heads2" Functional Components in the another functional component also known as component composition */}
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.querySelector('#root')); 
 
-root.render(heads); //calling the element in DOM  
+root.render(<HeadingComponent/>); //calling the function components
