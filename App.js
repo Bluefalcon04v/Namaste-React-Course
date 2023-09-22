abfbfe1cdd5287cf36173ff9c6ff947900377f7c
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2023-09-22 17:47:55 +0530

    exectue any JS code inside functional component

--- App.js
+++ App.js
@@ -7,9 +7,10 @@ const heads = (
 
 const Heads2 = () =>(
     <h2>Namaste heads2</h2>
-)
-
-const HeadingComponent = () =>( // Creating a functional component
+    )
+    
+    
+    const HeadingComponent = () =>( // Creating a functional component
     <div>
         <h1> Namaste Mom</h1>
         {heads}        {/* Calling for the "heads" element in the functional components */}
@@ -18,6 +19,7 @@ const HeadingComponent = () =>( // Creating a functional component
         {Heads2()}        {/* Calling for the "heads2" Functional Components in the another functional component also known as component composition*/}
         {/* OR */}
         <Heads2/>        {/* Calling for the "heads2" Functional Components in the another functional component also known as component composition */}
+        {console.log("Hello")} {/*We can also run JS in functional components */}
     </div>
 )
 
