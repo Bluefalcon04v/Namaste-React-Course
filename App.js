2985c6bdcb917cd06c3c205004cab102e878c3e2
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2023-10-04 14:18:21 +0530

    hard coded passing data

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 /* 
 todo Header
@@ -34,58 +34,68 @@ todo footer
 *   - Links 
 */
 
+// ---------------------------------------------------------------- Header
+const Title = () => (
+  <a href="/">
+    <img
+      className="logo"
+      src="https://etimg.etb2bimg.com/photo/63298781.cms"
+      alt="logo Image"
+    />
+  </a>
+);
 
-    // ---------------------------------------------------------------- Header
-    const Title = () =>(
-         <a href='/'>
-            <img className='logo' src="https://i.pinimg.com/originals/f2/9f/d9/f29fd9265cad42047ecc32ae11826d4a.png" alt='Logo'/>
-         </a>
-    )
+const Header = () => (
+  <>
+    <div className="listContainer">
+      <Title />
+      <ul>
+        <li>About</li>
+        <li>Courses</li>
+        <li>Blogs</li>
+        <li>Contact</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  </>
+);
 
-    const Header = () =>( 
-        <>
-        <div className='listContainer'>
-        <Title/>
-            <ul>
-                <li>About</li>
-                <li>Shop</li>
-                <li>Blogs</li>
-                <li>Contact</li>
-            </ul>
-        </div>
+// ---------------------------------------------------------------- Body
 
-        </>
-    )
+const ResturantCard = () => {
+  return (
+    <div className="card">
+      <img
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ykoshijaxlbqjtvcnlju"
+        alt=""
+      />
+      <h2>Burger King</h2>
+      <h3>Pizza, Burger</h3>
+      <h5>4.3 Stars</h5>
+    </div>
+  );
+};
 
+const Body = () => (
+  <div>
+    <ResturantCard />
+  </div>
+);
+// ---------------------------------------------------------------- Footer
+const Footer = () => (
+  <>
+    <h1>Footer</h1>
+  </>
+);
 
-    // ---------------------------------------------------------------- Body
-    const Body = () =>( 
-        <>
-        <h1>
+const AppComponent = () => (
+  <>
+    <Header />
+    <Body />
+    <Footer />
+  </>
+);
 
-        Body
-        </h1>
-        </>
-    )
-    // ---------------------------------------------------------------- Footer
-    const Footer = () =>( 
-        <>
-        <h1>
+const root = ReactDOM.createRoot(document.querySelector("#root"));
 
-        Footer
-        </h1>
-        </>
-    )
-
-    const AppComponent = () =>(
-        <>
-            <Header/>
-            <Body/>
-            <Footer/>
-        </>
-    )
-
-
-const root = ReactDOM.createRoot(document.querySelector('#root')); 
-
-root.render(<AppComponent/>); 
+root.render(<AppComponent />);
