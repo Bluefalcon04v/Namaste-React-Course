e67b22cfcbbff405c68b10dbe5c823e1191a0f79
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2023-10-08 21:13:11 +0530

    Export default

---
+++ src/components/Headers.js
@@ -0,0 +1,27 @@
+const Title = () => (
+    <a href="/">
+      <img
+        className="logo"
+        src="https://etimg.etb2bimg.com/photo/63298781.cms"
+        alt="logo Image"
+      />
+    </a>
+  );
+  
+  const Header = () => (
+    <>
+      <div className="listContainer">
+        <Title />
+        <ul>
+          <li>About</li>
+          <li>Courses</li>
+          <li>Blogs</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </>
+  );
+
+
+export default Header;  // exporting as default
