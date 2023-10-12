145374032acb5409ee4e5fa400af81e6901fac14
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2023-10-12 07:40:30 +0530

    using JS methods to store variables

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,20 @@
 import { resturantListData } from "../config";
-import ResturantCard from "./ResturantCard"
+import ResturantCard from "./ResturantCard";
 
+const searchText = "KFC" // In react we dont use these methods to store the variables because the rerender the component every time they get updated
 const Body = () => (
-    <div className="resturantList">
-      {
-          resturantListData.map((value) => {
-              return <ResturantCard {...value.info} key={value.info.id}/>
-          })
-      }
-  
+<>
+    <div className="searchBar">
+      <input type="text" className="searchInput" placeholder="Search" value={searchText} onChange={(e)=>console.log(e.target.value)}></input>
+      <button className="searchButton" >Search</button>
     </div>
-  );
 
-  export default Body;
+    <div className="resturantList">
+      {resturantListData.map((value) => {
+        return <ResturantCard {...value.info} key={value.info.id} />;
+      })}
+    </div>
+  </>
+);
+
+export default Body;
