036f86b0c033f6543a6ddbd8c9463e8a7e1c4ab9
amit_sah <101953634+Bluefalcon04v@users.noreply.github.com>
2023-10-13 13:02:06 +0000

    filter search

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,48 +1,53 @@
-import { resturantListData } from "../config";
 import ResturantCard from "./ResturantCard";
 import { useEffect, useState } from "react";
-
+import Shimmer_UI from "./Shimmer_UI"
 
 function filterData(searchText, resturants) {
   const filterValue = resturants.filter((value) =>
-    value.info.name.includes(searchText)
-  )
+    value?.info?.name?.toLowerCase().includes(searchText.toLowerCase())
+  );
   return filterValue;
 }
 
-
-
 const Body = () => {
-  const [resturants, setResturants] = useState([])
-  const [searchText, setSearchText] = useState();
+  const [allResturants, setAllResturants] = useState([])
+  const [filteredResturants, setFilteredResturants] = useState([])
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     getResturantListData();
-  }, []); // when it will load it will call the the method inside it only once
+  }, []);
 
-  async function getResturantListData() { // feching the APIs
+  async function getResturantListData() {
     const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6286706&lng=77.36402570000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
     const JSON = await data.json();
-    setResturants(JSON?.data?.cards[2].card.card.gridElements.infoWithStyle.restaurants);  // Settin up the data 
+    setAllResturants(JSON?.data?.cards[2].card.card.gridElements.infoWithStyle.restaurants);
   }
 
-  return (
+
+  if (!allResturants) return null;
+
+  // if(filteredResturants?.length === 0)
+  //   return <h1>No Resturant matches your filter!</h1>
+
+  return allResturants?.length === 0 ? (<Shimmer_UI />) : (
     <>
+      {console.log("render")}
       <div className="searchBarContainer">
         <input type="text" className="searchInput"
           placeholder="Search"
           value={searchText || ""}
           onChange={(e) => { setSearchText(e.target.value) }}></input>
         <button className="searchButton" onClick={() => {
-          const data = filterData(searchText, resturants);
-          setResturants(data)
+          const data = filterData(searchText, allResturants);
+          setFilteredResturants(data)
         }
         } >Search</button>
       </div >
 
       <div className="resturantList">
-        {resturants.map((value) => {
-          return <ResturantCard {...value.info} key={value.info.id} />;
+        {filteredResturants.map((value) => {
+          return (<ResturantCard {...value.info} key={value.info.id} />);
         })}
       </div>
     </>
---
+++ src/components/Shimmer_UI.js
@@ -0,0 +1,9 @@
+const Shimmer_UI = () =>{
+    return(
+        <>
+            <h1>Loading.............</h1>
+        </>
+    )
+}
+
+export default Shimmer_UI;
