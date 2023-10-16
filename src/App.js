f08d2bc788e3848244e15dfd5cad856a5e19ee8b
amit_sah <101953634+Bluefalcon04v@users.noreply.github.com>
2023-10-16 06:27:01 +0000

    Nested Routing by react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
-import React from "react";
+import React, { Children } from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./components/Headers"; 
+import Header from "./components/Headers";
 import Footer from "./components/Footer";
 import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const AppComponent = () => (
   <>
     <Header />
-    <Body />
+    {/* Outlet Component will be used for the mounting the change of the route */}
+    <Outlet /> 
     <Footer />
   </>
 );
 
 const appRouter = createBrowserRouter([
   {
-    path:"/",
-    element: <AppComponent/>,
-    errorElement: <Error/>
-  },
-  {
-    path:"/about",
-    element: <About/>
-  },
-  {
-    path: "/contact",
-    element: <Contact/>
-  }
-])
+    path: "/",
+    element: <AppComponent />,
+    errorElement: <Error />,
+    // children will be mounted on the Outlet position these are the Nested Routing
+    children: [
+      {
+        path: "/",
+        element: <Body />
+      },
+      {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "/contact",
+        element: <Contact />
+      }
+    ],
+  }])
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
