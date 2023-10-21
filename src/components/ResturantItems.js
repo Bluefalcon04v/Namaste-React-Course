f7a4db59fb7b2a138e5f3a7ac28f570a0496e21a
amit_kumar <amit.k1@kgs.live>
2023-10-21 17:00:06 +0530

    fixing CSS

--- src/components/ResturantItems.js
+++ src/components/ResturantItems.js
@@ -1,15 +1,17 @@
-import {IMG_CDN_URL} from "../config"
+import { IMG_CDN_URL } from "../config";
 
-const ResturantItems = ({name,category, description, imageId, price})=>{
-    return(
-        <div className="menuItems">
-            <img src={IMG_CDN_URL + imageId} alt="Image" />
-            <p>{name}</p>
-            <p>{category}</p>
-            <p>{description}</p>
-            <p>&#8377; {price/100}</p>
-        </div>
-    )
-}
+const ResturantItems = ({ name, category, description, imageId, price }) => {
+  return (
+    <div className="menuItems">
+      <img src={IMG_CDN_URL + imageId} alt="Image" />
+      <div className="menuItemsContent">
+        <h3>{name}</h3>
+        <p>{category}</p>
+        <p>{description}</p>
+        <p>&#8377; {price / 100}</p>
+      </div>
+    </div>
+  );
+};
 
 export default ResturantItems;
--- src/components/ResturantMenu.js
+++ src/components/ResturantMenu.js
@@ -1,63 +1,70 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { IMG_CDN_URL } from "../config"
+import { IMG_CDN_URL } from "../config";
 import ResturantItems from "./ResturantItems";
 
 const ResturantMenu = () => {
-    const [resturant, setResturant] = useState([])
-    const [resturantMenuItems, setResturantMenuItems] = useState([])
-    const [loading, setLoading] = useState(true)
-    const params = useParams();
-    const { resid } = params;
+  const [resturant, setResturant] = useState([]);
+  const [resturantMenuItems, setResturantMenuItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const params = useParams();
+  const { resid } = params;
 
-    useEffect(() => {
-        getResturantInfo();
-    }, [])
+  useEffect(() => {
+    getResturantInfo();
+  }, []);
 
-    async function getResturantInfo() {
-        try {
-            const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6286706&lng=77.36402570000001&restaurantId=51293&catalog_qa=undefined&submitAction=ENTER")
-            const json = await data.json()
-            setResturant(json?.data?.cards[0]?.card?.card?.info)
-            const allItems = [];  
-            json?.data?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.forEach((res) => {
-                res?.card?.card?.itemCards?.forEach((val) => {
-                    allItems.push(val?.card?.info);
-                });
-            });
-
-            setResturantMenuItems(allItems);
-            setLoading(false)
+  async function getResturantInfo() {
+    try {
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.6286706&lng=77.36402570000001&restaurantId=142444&catalog_qa=undefined&submitAction=ENTER"
+      );
+      const json = await data.json();
+      setResturant(json?.data?.cards[0]?.card?.card?.info);
+      const allItems = [];
+      json?.data?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.forEach(
+        (res) => {
+          res?.card?.card?.itemCards?.forEach((val) => {
+            allItems.push(val?.card?.info);
+          });
         }
-        catch (error) {
-            console.log("Fetching Error: ", error);
-            setLoading(false)
-        }
+      );
+
+      setResturantMenuItems(allItems);
+      setLoading(false);
+    } catch (error) {
+      console.log("Fetching Error: ", error);
+      setLoading(false);
     }
+  }
 
-    return (
-        <>
-            <div>
-                <img src={IMG_CDN_URL + resturant?.cloudinaryImageId}></img>
-                <h1>{resturant.name}</h1>
-                <h3>{resturant.areaName}</h3>
-                <h3>{resturant.city}</h3>
-                <h3>{resturant.costForTwoMessage}</h3>
-                <h4>{resturant.totalRatingsString}</h4>
-                <h1>Resturant id :{resid}</h1>
-            </div>
-            <div className="resturantMenuItems">
-            {loading ? (
-                    <p>Loading...</p>
-                ) : (
-                    resturantMenuItems.map((item, index) => (
-                        <ResturantItems {...item} key={item?.id && index} />
-                    ))
-                )}
-            </div>
-        </>
-    )
-}
-
+  return (
+    <>
+    <div className="resturantMenuMainContainer">
+    <img className="itemImageInBg" src={IMG_CDN_URL + resturant?.cloudinaryImageId}></img>
+      <div className="resturantMainDetailContainer">
+        <img src={IMG_CDN_URL + resturant?.cloudinaryImageId}></img>
+        <div>
+          <h1>{resturant.name}</h1>
+          <h3>{resturant.areaName}, {resturant.city}</h3>
+          <h3>{resturant.costForTwoMessage}</h3>
+          <h4>{resturant.totalRatingsString}</h4>
+          <h1>Resturant id :{resid}</h1>
+        </div>
+      </div>
+      <div className="resturantMenuItemsContainer">
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          resturantMenuItems.map((item, index) =>  {
+            return (<ResturantItems {...item} key={item?.id && index} />)
+          })
+            )}
+      </div>
+            {resturantMenuItems.map((item) => console.log(item))}
+    </div>
+    </>
+  );
+};
 
 export default ResturantMenu;
