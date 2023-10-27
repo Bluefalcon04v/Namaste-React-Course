aa3138ce470ae3fae982d732cda9c13c208638fa
amit_kumar <amit.k1@kgs.live>
2023-10-27 15:36:44 +0530

    Setting state in the class based component

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,16 +1,36 @@
 import React from "react";
 
 class UserClass extends React.Component {
-    render(props){ // Using Props in class based components 
-        return(
-            <div className="userCard">
-            User Class Component
-            <h3>Name = {this.props.name}</h3>
-            <h3>Occupation = {this.props.occupation}</h3>
-            <h3>City Name = {this.props.city}</h3>
-          </div>    
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {  // Setting multiple States
+      value: 0,
+      value2: 2,
+    };
+  }
+  render() {
+    return (
+      <div className="userCard">
+        User Class Component
+        <h3>Name = {this.props.name}</h3>
+        <h3>Occupation = {this.props.occupation}</h3>
+        <h3>City Name = {this.props.city}</h3>
+        <p>{this.state.value}</p>
+        <p>{this.state.value2}</p>
+
+        <button
+          onClick={() => {
+            this.setState({
+              value: 1,
+              value2: 7,
+            });
+          }}
+        >
+          Class Component
+        </button>
+      </div>
+    );
+  }
 }
 
 export default UserClass;
